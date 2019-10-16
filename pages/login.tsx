3b38581aee871f539e61467e909ae848f8696eca
Wei Lee <leewei48750@gmail.com>
2019-10-16 16:54:05 +0800

    feat(pages/): static pages

---
+++ pages/login.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Head from 'next/head';
+import Nav from '../components/nav';
+
+const Login = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel='icon' href='/static/favicon.ico' />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+    <Nav />
+    <style jsx global>{`
+      html, body {
+        margin: 0;
+      }
+      *, *::before, *::after  {
+        box-sizing: border-box;
+      }
+    `}</style>
+  </div>
+);
+
+export default Login;
