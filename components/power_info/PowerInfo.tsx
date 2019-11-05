6b393fe89170704bbe87e44c80bdf35b2e1da0b0
ttw225 <P76084106@ncku.edu.tw>
2019-11-05 20:21:34 +0800

    feat(power_info page):

---
+++ components/power_info/PowerInfo.tsx
@@ -0,0 +1,152 @@
+import 'date-fns';
+import React, { FunctionComponent, forwardRef, useEffect } from 'react';
+import dayjs from 'dayjs';
+import MaterialTable, { Column } from 'material-table';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import styled from 'styled-components';
+
+const PowerInfoHeaders = {
+  'Content-Type': 'application/json',
+  Authorization: 'Bearer xxxx',
+};
+
+const url_powerinfo = 'http://140.116.247.120:5000/power_info';
+
+interface PowerInfoData {
+  id: string;
+  date: string;
+  time: string;
+  power_display: number;
+  data_type: string;
+  address: string;
+}
+
+interface TableState {
+  columns: Array<Column<PowerInfoData>>;
+}
+
+const Field: TableState = {
+  columns: [
+    {
+      field: 'date',
+      title: '日期',
+      type: 'string',
+      cellStyle: {
+        textAlign: 'center',
+      },
+    },
+    {
+      field: 'time',
+      title: '紀錄時間',
+      type: 'string',
+      cellStyle: {
+        textAlign: 'center',
+      },
+    },
+    {
+      field: 'power_display',
+      title: '度數(kWh)',
+      type: 'string',
+      cellStyle: {
+        textAlign: 'center',
+      },
+    },
+    {
+      field: 'data_type',
+      title: '用產電種類',
+      type: 'string',
+      cellStyle: {
+        textAlign: 'center',
+      },
+    },
+    {
+      field: 'address',
+      title: '連結',
+      type: 'string',
+      cellStyle: {
+        textAlign: 'center',
+      },
+      render: rowData => (
+        <a target="_blank" href={rowData.address}>
+          URL
+        </a>
+      ),
+    },
+  ],
+};
+
+const DatePicker = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: end;
+`;
+
+function PowerInfoTable() {
+  const [state] = React.useState<TableState>(Field);
+  const [selectedDate, setSelectedDate] = React.useState<Date | null>(
+    new Date(dayjs().format('YYYY/MM/DD')),
+  );
+  // `tableRef` type finding
+  const tableRef = React.createRef<any>();
+
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date);
+    tableRef.current.onQueryChange();
+  };
+  return (
+    <>
+      <DatePicker>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <KeyboardDatePicker
+            margin="normal"
+            id="date-picker-dialog"
+            label="Choose Data Date"
+            format="yyyy/MM/dd"
+            value={selectedDate}
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+        </MuiPickersUtilsProvider>
+      </DatePicker>
+      <MaterialTable
+        title={'電力資訊'}
+        options={{
+          search: false,
+          headerStyle: {
+            textAlign: 'center',
+          },
+        }}
+        tableRef={tableRef}
+        columns={state.columns}
+        data={query =>
+          new Promise((resolve, reject) => {
+            let url = url_powerinfo;
+            url += '?per_page=' + query.pageSize;
+            url += '&page=' + (query.page + 1);
+            url += '&time=' + dayjs(selectedDate).format('YYYY/MM/DD');
+            fetch(url, {
+              method: 'get',
+              headers: new Headers(PowerInfoHeaders),
+            })
+              .then(response => response.json())
+              .then(result => {
+                resolve({
+                  data: result.data,
+                  page: result.page - 1,
+                  totalCount: result.totalCount,
+                });
+              });
+          })
+        }
+      />
+    </>
+  );
+}
+
+export default PowerInfoTable;
--- pages/power_info.tsx
+++ pages/power_info.tsx
@@ -1,29 +1,11 @@
 import React from 'react';
 import Page from '../components/Page';
-import Head from 'next/head';
-import Nav from '../components/nav';
-import { PowerType } from '../typing/PowerInfoType';
-import { PowerInfoList } from '../components/power_info/PowerInfoList';
 
-const state = {
-  powerInfos: [{
-    date: "2019/10/18",
-    time: "18:19",
-    usage: "0.01",
-    type: PowerType.Homepage,
-    url: "https://www.google.com"
-  }, {
-    date: "2019/10/18",
-    time: "18:19",
-    usage: "0.01",
-    type: PowerType.Homepage,
-    url: "https://www.google.com"
-  }]
-};
+import PowerInfoTable from '../components/power_info/PowerInfo';
 
 const PowerInfo = () => (
   <Page title="電力資訊">
-    <PowerInfoList infos={state.powerInfos} />
+    <PowerInfoTable />
   </Page>
 );
 
