249140ec819f3bded4ebf789bda42a4e00116867
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2019-11-06 17:19:47 +0800

    refactor(pages/index.tsx -> home/index.tsx): feat home/index.tsx and move components into it

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
 import Page from '../components/Page';
-import CurrentTimeBlock from '../components/home/CurrentTimeBlock';
-import BiddingInfo from '../components/home/BiddingInfo';
-import GraphContainer from '../components/home/GraphContainer';
-
-const UpperContainer = styled.div`
-  width: 100%;
-  height: 336px;
-  display: flex;
-  flex-direction: row;
-  margin-top: -70px;
-`;
+import HomeContainer from '../components/home';
 
 const Home = () => (
   <Page title="首頁">
-    <UpperContainer>
-      <CurrentTimeBlock></CurrentTimeBlock>
-      <BiddingInfo></BiddingInfo>
-    </UpperContainer>
-    <GraphContainer />
+    <HomeContainer />
   </Page>
 );
 
