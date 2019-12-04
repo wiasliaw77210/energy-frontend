53b31a6adbd0892395c28739e5f5b5452c7bf529
Wei Lee <leewei48750@gmail.com>
2019-12-04 10:01:09 +0800

    refactor(layout.tsx): move Page to layout

--- layout/layout.tsx
+++ layout/layout.tsx
@@ -1,32 +1,70 @@
-import React, { PropsWithChildren } from 'react';
+import React from 'react';
 import Head from 'next/head';
+import Navbar from '../components/nav';
 
-interface IProps {
+type TProps = {
   title: string;
-}
+  children?: React.ReactNode;
+};
 
-export default ((props: PropsWithChildren<IProps>) => {
+const Page: React.FC<TProps> = ({ title, children }) => {
   return (
     <div>
       <Head>
-        <title>{props.title}</title>
+        <title>{title}</title>
         <link rel="icon" href="/static/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <div id="root">{props.children}</div>
+      <div className="container">
+        <Navbar />
+        <div className="main">
+          <h1 className="title">{title}</h1>
+          <div className="content">{children}</div>
+        </div>
+      </div>
       <style jsx global>{`
         html,
         body {
           margin: 0;
-          font-size: 24px;
+          font-family: 'Roboto', sans-serif;
+          width: 100%;
         }
-
         *,
         *::before,
         *::after {
           box-sizing: border-box;
         }
+
+        .container {
+          display: flex;
+        }
+
+        .title {
+          font-size: 40px;
+          line-height: 1.18;
+          color: #707070;
+          font-weight: normal;
+          margin: 20px 0 20px 0;
+        }
+
+        .main {
+          display: flex;
+          padding: 94px 64px;
+          flex: 1;
+          flex-direction: column;
+          background-color: #f1f2f1;
+        }
       `}</style>
     </div>
   );
-}) as React.FC<PropsWithChildren<IProps>>;
+};
+
+export default Page;
