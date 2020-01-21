fd6b4f584ef9216b6bc2209c57f1a3401184033d
Wei Lee <leewei48750@gmail.com>
2020-01-21 14:35:18 +0800

    refactor(pages/login.tsx): refactor login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,45 +1,58 @@
 import React from 'react';
-import Head from 'next/head';
-
+import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
+import Layout from '../layout/loginLayout';
 import LoginForm from '../components/loginForm';
+import LoginPic from '../static/login/login_pic.png';
 
-const Login: React.FC = () => (
-  <div>
-    <Head>
-      <title>Login</title>
-      <link rel="icon" href="/static/favicon.ico" />
-      <link
-        href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
-        rel="stylesheet"
+const Block = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Login: React.FC = () => {
+  const { t } = useTranslation();
+  return (
+    <Layout title={t('login.title')}>
+      <Block
+        style={{
+          backgroundImage: `url(${LoginPic})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          float: 'left',
+        }}
       />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </Head>
-    <main>
-      <LoginForm />
-    </main>
-    <style jsx global>{`
-      html,
-      body {
-        margin: 0;
-        font-family: 'Roboto', sans-serif;
-        width: 100%;
-        overflow-x: hidden;
-      }
-      *,
-      *::before,
-      *::after {
-        box-sizing: border-box;
-      }
-      main {
-        width: 100vw;
-        height: 100vh;
-      }
-    `}</style>
-  </div>
-);
+      <Block
+        style={{
+          float: 'right',
+          backgroundColor: '#f1f2f1',
+        }}
+      >
+        <LoginForm />
+      </Block>
+      <style jsx global>{`
+        html,
+        body {
+          margin: 0;
+          font-family: Roboto, sans-serif;
+          width: 100%;
+          overflow-x: hidden;
+        }
+        *,
+        *::before,
+        *::after {
+          box-sizing: border-box;
+        }
+        main {
+          width: 100vw;
+          height: 100vh;
+        }
+      `}</style>
+    </Layout>
+  );
+};
 
 export default Login;
