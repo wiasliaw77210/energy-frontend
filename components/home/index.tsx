33424969d8222cf4796ed2781fbd5b6863d23a67
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2020-03-04 14:37:32 +0800

    feat(components/home): complete homepage canvas

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -19,7 +19,7 @@ const HomeContainer: FunctionComponent = () => {
         <CurrentTimeBlock />
         <BiddingInfo />
       </Container>
-      {/* <GraphContainer /> */}
+      <GraphContainer />
     </div>
   );
 };
